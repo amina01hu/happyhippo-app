fdba683a29de7fb5ec6b45d3c531ec40fba4721a
Samiya Hussein <samiyahussein@samiyas-air.home>
2023-01-28 21:54:54 -0500

    added navbar

---
+++ aminaeshop/src/components/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title:'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title:'About Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title:'Github',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title:'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    }
+
+]
